0bc8b459e415ea0058b4180862a97e32003a7f33
Alro1228 <alro_luo@163.com>
2022-03-08 22:35:56 +0800

    test

--- aspnetapp/Model.cs
+++ aspnetapp/Model.cs
@@ -7,4 +7,11 @@ namespace aspnetapp
         public DateTime createdAt { get; set; }
         public DateTime updatedAt { get; set; }
     }
+
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+    }
 }
